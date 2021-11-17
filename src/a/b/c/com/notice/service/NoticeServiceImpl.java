b6ef62d059c88c85a50c6237bc39398f48b55ef2
LOVEBADA9686 <lovebada9686@gmail.com>
2021-11-17 19:31:53 +0900

    20211117_1

--- src/a/b/c/com/notice/service/NoticeServiceImpl.java
+++ src/a/b/c/com/notice/service/NoticeServiceImpl.java
@@ -1,49 +1,49 @@
-package a.b.c.com.notice.service;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import a.b.c.com.notice.dao.NoticeDAO;
-import a.b.c.com.notice.vo.NoticeVO;
-
-public class NoticeServiceImpl implements NoticeService {
-	
-	private NoticeDAO noticeDAO;
-	
-	@Autowired(required=false)
-	public NoticeServiceImpl(NoticeDAO noticeDAO) {
-		this.noticeDAO = noticeDAO;
-	}
-
-	@Override
-	public List<NoticeVO> noticeSelect(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<NoticeVO> noticeSelectAll(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int noticeInsert(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int noticeUpdate(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int noticeDelete(NoticeVO nvo) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-}
+//package a.b.c.com.notice.service;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import a.b.c.com.notice.dao.NoticeDAO;
+//import a.b.c.com.notice.vo.NoticeVO;
+//
+//public class NoticeServiceImpl implements NoticeService {
+//	
+//	private NoticeDAO noticeDAO;
+//	
+//	@Autowired(required=false)
+//	public NoticeServiceImpl(NoticeDAO noticeDAO) {
+//		this.noticeDAO = noticeDAO;
+//	}
+//
+//	@Override
+//	public List<NoticeVO> noticeSelect(NoticeVO nvo) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public List<NoticeVO> noticeSelectAll(NoticeVO nvo) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public int noticeInsert(NoticeVO nvo) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	@Override
+//	public int noticeUpdate(NoticeVO nvo) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	@Override
+//	public int noticeDelete(NoticeVO nvo) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//}
